408f049e9ffbab39b0da543b1728ee78d83d1fd9
elkaRD <R.Dudzinski@stud.elka.pw.edu.pl>
2019-05-11 17:52:27 +0200

    added fragments and navigation activity

---
+++ app/src/main/java/com/nieelitarni/besthackathon2019/CommitsFragment.java
@@ -0,0 +1,22 @@
+package com.nieelitarni.besthackathon2019;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CommitsFragment extends Fragment
+{
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.fragment_commits, container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/nieelitarni/besthackathon2019/NavigationActivity.java
@@ -0,0 +1,65 @@
+package com.nieelitarni.besthackathon2019;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class NavigationActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment fragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.navigation_progress:
+                    fragment = new ProgressFragment();
+                    break;
+                case R.id.navigation_tasks:
+                    fragment = new TasksFragment();
+                    break;
+                case R.id.navigation_users:
+                    fragment = new UsersFragment();
+                    break;
+                case R.id.navigation_commits:
+                    fragment = new CommitsFragment();
+                    break;
+            }
+            replaceFragment(fragment);
+
+            return true;
+        }
+    };
+
+    public void replaceFragment(Fragment fragment)
+    {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameContainer, fragment);
+        fragmentTransaction.addToBackStack(fragment.toString());
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
