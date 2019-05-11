7707b807c39371f938de7591d5a32b414eb1c089
elkaRD <R.Dudzinski@stud.elka.pw.edu.pl>
2019-05-11 20:13:10 +0200

    added FileIO class

--- app/src/main/java/com/nieelitarni/besthackathon2019/FileIO.java
+++ app/src/main/java/com/nieelitarni/besthackathon2019/FileIO.java
@@ -1,6 +1,41 @@
 package com.nieelitarni.besthackathon2019;
 
+import android.content.Context;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileIO
 {
+    public static void save(Context context, String fileName, String json)
+    {
+        try {
+            FileWriter file = new FileWriter("/data/" + context.getPackageName() + "/" + fileName);
+            file.write(json);
+            file.flush();
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public static String read(Context context, String fileName) {
+        String result = null;
+        try {
+            File f = new File("/data/" + context.getPackageName() + "/" + fileName);
+            FileInputStream is = new FileInputStream(f);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            result = new String(buffer);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
 }
