79ab0635c7a02656f2c35220746448a9a9e93591
elkaRD <R.Dudzinski@stud.elka.pw.edu.pl>
2019-05-12 00:39:12 +0200

    added lists of commits and users for a single task

--- app/src/main/java/com/nieelitarni/besthackathon2019/TaskCommitsActivity.java
+++ app/src/main/java/com/nieelitarni/besthackathon2019/TaskCommitsActivity.java
@@ -2,10 +2,18 @@ package com.nieelitarni.besthackathon2019;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.ScaleAnimation;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class TaskCommitsActivity extends AppCompatActivity
 {
     private Task task;
+    private ArrayList<View> displayedItems = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +23,44 @@ public class TaskCommitsActivity extends AppCompatActivity
         Bundle extras = getIntent().getExtras();
         String taskId = extras.getString(TasksFragment.INTENT_PARAM);
         task = AppManager.getInstance().getTaskById(taskId);
+
+        updateItemsList();
+    }
+
+    private void updateItemsList()
+    {
+        for (View toRemove : displayedItems)
+            ((ViewGroup) toRemove.getParent()).removeView(toRemove);
+
+        displayedItems.clear();
+
+        ViewGroup itemsViewGroup = (ViewGroup) findViewById(R.id.itemsLayout);
+
+        ArrayList<Commit> commits = task.getCommits();
+
+        for (Commit commit : commits)
+        {
+            View child = LayoutInflater.from(this).inflate(R.layout.item_task_commit, null);
+
+            TextView name = child.findViewById(R.id.textViewDetails);
+            name.setText(commit.getContent());
+
+            TextView hash = child.findViewById(R.id.textViewHash);
+            hash.setText(commit.getHash());
+
+            TextView timeAgo = child.findViewById(R.id.textViewDate);
+            timeAgo.setText(DateHandler.getTimeAgo(commit.getTime()));
+
+            TextView author = child.findViewById(R.id.textViewAuthor);
+            author.setText(commit.getAuthor());
+
+            itemsViewGroup.addView(child);
+            displayedItems.add(child);
+
+            ScaleAnimation anim = new ScaleAnimation(0,1,0,1);
+            anim.setDuration(200);
+            anim.setFillAfter(true);
+            child.startAnimation(anim);
+        }
     }
 }
--- app/src/main/java/com/nieelitarni/besthackathon2019/TaskUsersActivity.java
+++ app/src/main/java/com/nieelitarni/besthackathon2019/TaskUsersActivity.java
@@ -2,9 +2,17 @@ package com.nieelitarni.besthackathon2019;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.ScaleAnimation;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class TaskUsersActivity extends AppCompatActivity {
     private Task task;
+    private ArrayList<View> displayedItems = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +22,35 @@ public class TaskUsersActivity extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         String taskId = extras.getString(TasksFragment.INTENT_PARAM);
         task = AppManager.getInstance().getTaskById(taskId);
+
+        updateItemsList();
+    }
+
+    private void updateItemsList()
+    {
+        for (View toRemove : displayedItems)
+            ((ViewGroup) toRemove.getParent()).removeView(toRemove);
+
+        displayedItems.clear();
+
+        ViewGroup tasksViewGroup = (ViewGroup) findViewById(R.id.itemsLayout);
+
+        ArrayList<User> users = task.getUsers();
+
+        for (User user : users)
+        {
+            View child = LayoutInflater.from(this).inflate(R.layout.item_task_user, null);
+
+            TextView name = child.findViewById(R.id.textViewUserName);
+            name.setText(user.getName());
+
+            tasksViewGroup.addView(child);
+            displayedItems.add(child);
+
+            ScaleAnimation anim = new ScaleAnimation(0,1,0,1);
+            anim.setDuration(200);
+            anim.setFillAfter(true);
+            child.startAnimation(anim);
+        }
     }
 }
