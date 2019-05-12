93ce4e5697263252d69b0878662156c29d424da7
elkaRD <R.Dudzinski@stud.elka.pw.edu.pl>
2019-05-12 04:52:22 +0200

    displaying info about selected task and marking it as done

--- app/src/main/java/com/nieelitarni/besthackathon2019/TaskInfoActivity.java
+++ app/src/main/java/com/nieelitarni/besthackathon2019/TaskInfoActivity.java
@@ -2,6 +2,8 @@ package com.nieelitarni.besthackathon2019;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class TaskInfoActivity extends AppCompatActivity {
 
@@ -15,5 +17,26 @@ public class TaskInfoActivity extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         String taskId = extras.getString(TasksFragment.INTENT_PARAM);
         task = AppManager.getInstance().getTaskById(taskId);
+
+        TextView title = findViewById(R.id.textViewTitle);
+        title.setText(task.getTitle());
+
+        TextView id = findViewById(R.id.textViewId);
+        id.setText("ID: " + task.getId());
+
+        TextView date = findViewById(R.id.textViewDate);
+        date.setText("Created " + DateHandler.getTimeAgo(task.getCreateDate()));
+
+        TextView users = findViewById(R.id.textViewUsers);
+        users.setText("Contributors: " + task.getUsers().size());
+
+        TextView details = findViewById(R.id.textViewDetails);
+        details.setText("Details: " + task.getDescription());
+    }
+
+    public void onClickMarkAsDone(View view)
+    {
+        task.setStatus(TaskStatus.Completed);
+        finish();
     }
 }
